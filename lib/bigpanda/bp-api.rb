f0bbff3014cd03a63f652afb9acd93ee8b4e87d7
Shmeff <shmeff@bigpanda.io>
2013-07-17 19:55:08 +0300

    Changing all string based keys to be symbol based.

--- lib/bigpanda/bp-api.rb
+++ lib/bigpanda/bp-api.rb
@@ -8,10 +8,10 @@ module BigPanda
 
   DEFAULT_CONFIG_FILES  = [ "/etc/bigpanda.yaml", "/etc/bigpanda.yml" ]
   DEFAULT_CONFIGURATION = { # overridable via the configuration file
-    'access_token'          => nil,
-    'target_url'            => 'https://api.bigpanda.io',
-    'deployment_start_path' => '/data/events/deployments/start',
-    'deployment_end_path'   => '/data/events/deployments/end'
+    :access_token          => nil,
+    :target_url            => 'https://api.bigpanda.io',
+    :deployment_start_path => '/data/events/deployments/start',
+    :deployment_end_path   => '/data/events/deployments/end'
   }
 
   #
@@ -20,20 +20,17 @@ module BigPanda
   # The client needs to be initialized with the access_token of the organization.
   # Possible options:
   # 1. Pass the access_token as a :access_token parameter
-  # 2. Place a bigpanda.yaml file in /etc/ and add to the file 'access_token': YOUR_TOKEN
+  # 2. Place a bigpanda.yaml file in /etc/ and add to the file -  'access_token': YOUR_TOKEN
   # 3. Pass a location of a yaml file using the :file parameter
-  # Can be constructed from a Hash with the used :access_token or from a file by passing the file location
+  #
   #
   class Client
     attr_reader :config, :ssl
 
     def initialize(options = {})
 
-      options['access_token'] ||= options[:access_token] # Take the string or symbol key
-
-      file = options.delete(:file) || options.delete('file')
-
-      unless options.fetch('access_token', nil)
+      unless options.fetch(:access_token, nil)
+        file = options.delete(:file)
         file_config, config_files = read_config_from_file(file)
         options.merge!(file_config) if file_config
       end
@@ -41,17 +38,17 @@ module BigPanda
       @config = DEFAULT_CONFIGURATION.merge(options)
       @ssl = options.delete(:ssl) || {}
 
-      unless @config.fetch('access_token', nil)
+      unless @config.fetch(:access_token, nil)
         raise "No BigPanda config token received, and no configuration file found. Searched: #{config_files.join ','}."
       end
     end
 
     def deployment_start(options = {})
-      return deployment_notification config['deployment_start_path'], options
+      return deployment_notification config[:deployment_start_path], options
     end
 
     def deployment_end(options = {})
-      return deployment_notification config['deployment_end_path'], options
+      return deployment_notification config[:deployment_end_path], options
     end
 
     private
@@ -69,10 +66,11 @@ module BigPanda
 
       # Perform the actual request to BigPanda API
       #
-      def request(path, body, target_url = config['target_url'])
+      def request(path, body, target_url = config[:target_url])
         headers  = { 'Accept' => 'application/json',
                      'Content-Type' => 'application/json',
-                     'Authorization' => "Bearer #{config['access_token']}" }
+                     'Authorization' => "Bearer #{config[:access_token]}" }
+
         uri = URI.parse(target_url)
         h = Net::HTTP.new uri.host, uri.port
         h.set_debug_output $stderr if $DEBUG
@@ -112,6 +110,11 @@ module BigPanda
         config_files.each do |config_file|
           if File.exists? config_file
             config = YAML.load_file(config_file)
+
+            # Convert string keys to symbols
+            config.keys.each do |key|
+              config[(key.to_sym rescue key) || key] = config.delete(key)
+            end
           end
         end
 
